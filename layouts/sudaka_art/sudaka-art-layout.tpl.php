badab73664452b99ec09fa2dfd94f6fd9bc9c8b7
dpineiden <dahalpi@gmail.com>
2014-11-05 21:42:19 -0300

    Se reconfigura layour de articulos: sudaka-art

--- layouts/sudaka_art/sudaka-art-layout.tpl.php
+++ layouts/sudaka_art/sudaka-art-layout.tpl.php
@@ -1,7 +1,7 @@
 <div<?php print $attributes; ?>>
   <header  role="banner">
-    <div class="l-wm-art-header">
-    <div class="l-wm-art-branding" role="logo-marca">
+    <div class="l-sk-art-header">
+    <div class="l-sk-art-branding" role="logo-marca">
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo" role="logo-sitio"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
       <?php endif; ?>
@@ -17,30 +17,30 @@
         <?php endif; ?>
       <?php endif; ?>
       <?php print render($page['branding']); ?></div>
-        <div class="l-wm-art-principal" role="principal">
+        <div class="l-sk-art-principal" role="principal">
         <?php print render($page['principal']); ?>
         </div>
       </div>        
   </header> 
   <body>
-    <div class="l-wm-art-slider" role="banner">
+    <div class="l-sk-art-slider" role="banner">
      <?php print render($page['banner']); ?>
     </div>	  
 	
-    <div class="l-wm-art-nav" role="navegacion">
+    <div class="l-sk-art-nav" role="navegacion">
 		<?php print render($page['navigation']); ?>
 	  </div>		
 	
-    <div class="l-wm-art-region--destacado" role="destacado">
+    <div class="l-sk-art-region--destacado" role="destacado">
       <?php print render($page['highlighted']); ?>
     </div>
     
-    <div class="l-wm-art-region--breadcrumb" role="breadcrumb">
+    <div class="l-sk-art-region--breadcrumb" role="breadcrumb">
       <?php print $breadcrumb; ?>  
     </div>
-<div class="l-wm-art--region-content" role"Region de Contenidos">
+<div class="l-sk-art--region-content" role"Region de Contenidos">
 
-  <div class="l-wm-art-content" role="main">        
+  <div class="l-sk-art-content" role="main">        
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
@@ -56,22 +56,22 @@
       <?php print render($page['content']); ?>
       <?php print $feed_icons; ?></div>
     
-  <div class="l-wm-art-sidebar-right" role="Lateral Derecha">
+  <div class="l-sk-art-sidebar-right" role="Lateral Derecha">
     <?php print render($page['sidebar_right']); ?></div>
    
     </div>
 
 
-  <div  class="l-wm-art--region-footer" role="Info Footer">
-    <div class="l-wm-art-region-prefooter">
-    <div class="l-wm-art-prefooter1" role="Pre Footer-1">
+  <div  class="l-sk-art--region-footer" role="Info Footer">
+    <div class="l-sk-art-region-prefooter">
+    <div class="l-sk-art-prefooter1" role="Pre Footer-1">
     <?php print render($page['pre_footer_1']); ?></div>
-      <div class="l-wm-art-prefooter2" role="Pre Footer-2">
+      <div class="l-sk-art-prefooter2" role="Pre Footer-2">
     <?php print render($page['pre_footer_2']); ?></div>
-      <div class="l-wm-art-prefooter3" role="Pre Footer-3">
+      <div class="l-sk-art-prefooter3" role="Pre Footer-3">
     <?php print render($page['pre_footer_3']); ?></div>
     </div>
-    <div class="l-wm-art-footer">
+    <div class="l-sk-art-footer">
     <?php print render($page['footer']); ?></div>
   </div>
 
